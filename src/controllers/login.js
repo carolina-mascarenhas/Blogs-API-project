eb062bc9f6f0a7e69fb4d3c6351f188aaad53740
Carolina Mascarenhas <carolina.mascarenhass96@hotmail.com>
2022-06-23 11:16:37 -0300

    fix: retirando password do payload do token

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -10,18 +10,14 @@ const router = express.Router();
 router.post('/', middlewares.loginValidation, async (req, res) => {
   const { email, password } = req.body;
 
-  const users = await userServices.getUser(email, password);
-  // console.log('console de users:', users);
-
-  if (users.length === 0) {
-    return res.status(400).json({ message: 'Invalid fields' });
-  }
+  const [users] = await userServices.getUser(email, password);
+  console.log('console de users:', users.dataValues);
 
   const jwtConfig = {
     algorithm: 'HS256',
   };
   
-  const token = jwt.sign({ data: users }, secret, jwtConfig);
+  const token = jwt.sign({ data: users.dataValues }, secret, jwtConfig);
   res.status(200).json({ token });
 });
 
--- src/services/login.js
+++ src/services/login.js
@@ -1,9 +1,19 @@
 const { User } = require('../database/models');
 
-const getUser = (email, password) => User.findAll({
-  attributes: ['email', 'password'],
-  where: { email, password },
-});
+const getUser = async (email, password) => {
+  const user = await User.findAll({
+    attributes: { exclude: ['password'] },
+    where: { email, password },
+  });
+
+  if (user.length === 0) {
+    const error = new Error('Invalid fields');
+    error.status = 400;
+    throw Object.assign(error);
+  }
+
+  return user;
+};
 
 module.exports = {
   getUser,
